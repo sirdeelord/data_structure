2db0faf095d781f5f8f9bdb146a9863e74f48b92
sirdeelord <saddyodus@yahoo.com>
2021-09-17 13:34:38 -0400

    introduced generic to accomotade any types

--- ds_code/src/SList.java
+++ ds_code/src/SList.java
@@ -1,55 +1,55 @@
-public class SList {
+public class SList<T> {
     /** nested class */
-    public static class IntNode {
-        public int item;
-        public IntNode next;
+    private class Node {
+        public T item;
+        public Node next;
     
-        public IntNode(int i, IntNode n) {
+        public Node(T i, Node n) {
             item = i;
             next = n;
         }
     }
 
     /** the first item (if it exists) is at sentinel.next  */
-    private IntNode sentinel;
+    private Node sentinel;
     private int size;
 
     /** create an empty SList */
     public SList() {
-        sentinel = new IntNode(63, null);
+        sentinel = new Node(null, sentinel);
         size = 0;
     }
 
     /** contructor with param */
-    public SList(int x) {
-        sentinel = new IntNode(63, null);
-        sentinel.next = new IntNode(x, null);
+    public SList(T x) {
+        sentinel = new Node(x, null);
+        sentinel.next = new Node(x, null);
         size = 1;
     }
 
 
     /** add x to the first item in the list */
-    public void addFirst(int x) {
-        sentinel.next = new IntNode(x, sentinel.next);
+    public void addFirst(T x) {
+        sentinel.next = new Node(x, sentinel.next);
         size += 1;
     }
 
     /** return the first item in the list */
-    public int getFirst() {
+    public T getFirst() {
         return sentinel.next.item;
     }
 
     /** add an item to the end of the list */
-    public void addLast(int x) {
+    public void addLast(T x) {
         size += 1;
 
-        IntNode p = sentinel;
+        Node p = sentinel;
         /* move p until it reaches the end of the list */
         while (p.next != null) {
             p = p.next;
         }
 
-        p.next = new IntNode(x, null);
+        p.next = new Node(x, null);
     }
 
     public int size() {
@@ -58,11 +58,11 @@ public class SList {
 
     public static void main(String[] args) {
         /*create a list of one integer, namely 10 */
-        SList L = new SList();
+        SList<Integer> L = new SList<>();
         L.addFirst(10);
         L.addFirst(5);
         L.addLast(20);
-        System.out.println(L.size());
+        System.out.println(L.getFirst());
     }
 }
 
