7410f8a35597b71b6b968f220293c7c0f2e8e092
sirdeelord <saddyodus@yahoo.com>
2021-09-16 23:17:49 -0400

    initial commit

---
+++ ds_code/src/IntList.java
@@ -0,0 +1,32 @@
+public class IntList {
+    public int first;
+    public IntList rest;        
+
+    public IntList(int f, IntList r) {
+        first = f;
+        rest = r;
+    }
+
+    public static void main(String[] args) throws Exception {
+
+    }
+
+    /** Return the size of the list using... recursion! */
+    public int size() {
+        if (rest == null) {
+            return 1;
+        }
+        return 1 + this.rest.size();
+    }
+
+    /** Return the size of the list using no recursion! */
+    public int iterativeSize() {
+        IntList p = this;
+        int totalSize = 0;
+        while (p != null) {
+            totalSize += 1;
+            p = p.rest;
+        }
+        return totalSize;
+    }
+}
