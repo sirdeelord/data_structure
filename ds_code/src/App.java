99895e3f4f71a7cd20b259068814833d0b5f2b54
sirdeelord <saddyodus@yahoo.com>
2021-10-04 22:43:11 -0400

    max consecutive 1s

--- ds_code/src/App.java
+++ ds_code/src/App.java
@@ -1,5 +1,31 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello World!");
+        int[] arr = {1,1,0,1,1,1};
+        System.out.println(findMaxConsecutiveOnes(arr));
+    }
+
+    public static int findMaxConsecutiveOnes(int[] nums) {
+        // Hint: Initialise and declare a variable here to 
+        // keep track of how many 1's you've seen in a row.
+        int max = 0;
+        int current = 0;
+        for (int i = 0; i < nums.length; i++) {
+
+        // for (int i : nums) {
+            // if (i == 1) {
+
+            if (nums[i] == 1) {
+                current++;
+            } else {
+                if (current > max) {
+                    max = current;
+                }
+                if (max >= nums.length / 2) {
+                    return max;
+                }
+                current = 0;
+            }
+        }
+        return Math.max(max, current);
     }
 }
