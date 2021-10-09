d3ffc4ee1c3bb4519ee5bdec5d7c7295fc898034
sirdeelord <saddyodus@yahoo.com>
2021-10-09 01:10:36 -0400

    update code

--- ds_code/src/App.java
+++ ds_code/src/App.java
@@ -5,27 +5,44 @@ public class App {
     }
 
     public static int findMaxConsecutiveOnes(int[] nums) {
-        // Hint: Initialise and declare a variable here to 
-        // keep track of how many 1's you've seen in a row.
-        int max = 0;
-        int current = 0;
+        int max = -1;
+        int curr = 0;
         for (int i = 0; i < nums.length; i++) {
-
-        // for (int i : nums) {
-            // if (i == 1) {
-
-            if (nums[i] == 1) {
-                current++;
-            } else {
-                if (current > max) {
-                    max = current;
-                }
-                if (max >= nums.length / 2) {
-                    return max;
-                }
-                current = 0;
+            if (nums[i] == 1) 
+                curr++;
+            else {
+                max = Math.max(max,curr);
+                curr = 0;
             }
         }
-        return Math.max(max, current);
+        max = Math.max(max,curr);
+        return max;
     }
+
+
+    // slower version
+    // public static int findMaxConsecutiveOnes(int[] nums) {
+    //     // Hint: Initialise and declare a variable here to 
+    //     // keep track of how many 1's you've seen in a row.
+    //     int max = 0;
+    //     int current = 0;
+    //     for (int i = 0; i < nums.length; i++) {
+
+    //     // for (int i : nums) {
+    //         // if (i == 1) {
+
+    //         if (nums[i] == 1) {
+    //             current++;
+    //         } else {
+    //             if (current > max) {
+    //                 max = current;
+    //             }
+    //             if (max >= nums.length / 2) {
+    //                 return max;
+    //             }
+    //             current = 0;
+    //         }
+    //     }
+    //     return Math.max(max, current);
+    // }
 }
